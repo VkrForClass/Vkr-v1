9463b9385f068df0d32598842d8639bb5ccb82d3
LROOP2022 <116558329+LROOP2022@users.noreply.github.com>
2024-05-17 11:52:59 +0300

    Добавлено меню для переодов

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,12 @@
 const swiperTop = new Swiper('.top__swiper', {
   // Optional parameters
   effect: 'fade',
+/*
   autoplay: {
   delay: 3500,
   disableOnInteraction: false,
   },
+*/
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
@@ -32,4 +34,16 @@ const  swiper = new Swiper(".period__slider", {
     clickable: true,
   },
 });
+
+// Добавим закрытие меню при повторном нажатии на кнопку
+document.getElementById('toggleNav').addEventListener('click', function(event) {
+    var mainNav = document.getElementById('mainNav');
+    if (mainNav.style.display === 'none') {
+      mainNav.style.display = 'flex';
+    } else {
+      mainNav.style.display = 'none';
+    }
+
+});
+
 
